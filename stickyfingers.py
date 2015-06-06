9ac5db862e95ba47fcd2318c4c76b42ae1094884
StevesAMonkey <steve@tuxcon.com>
2015-06-06 21:53:13 +0930

    Got the fingerprinting of DER happening!

--- stickyfingers.py
+++ stickyfingers.py
@@ -1,15 +1,22 @@
 #Import some libs and stuff
 import ssl
 import hashlib
-#from socket import socket
+from binascii import a2b_base64
 
+
+#Set the scene for our hashes
 sha256 = hashlib.sha256()
 
-#s = socket()
 #Define our host and port
 #TODO we'll be calling this via command line
-Host = ('host', 6697)
-HostCert = ssl.get_server_certificate(Host).encode('utf-8')
-sha256.update(HostCert)
+Host = ('hosthosthosthosthost', 6697)
+#Grab the certificate and encode in utf-8
+HostCert = ssl.get_server_certificate(Host)  #.encode('utf-8')
+#Convert from PEM to DER
+lines = HostCert.replace(" ",'').split()
+der = a2b_base64(''.join(lines[1:-1]))
+#Checksum the certificate
+sha256.update(der)
+#Make it nice and printable, then show it
 PrintableCert = sha256.hexdigest()
 print(PrintableCert)
