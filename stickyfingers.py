ba84b2e6cceeef92432a83651a2e58463e99ea07
StevesAMonkey <steve@tuxcon.com>
2015-06-06 22:35:11 +0930

    Make it a bit neater - remove old stuff

--- stickyfingers.py
+++ stickyfingers.py
@@ -14,30 +14,26 @@ class Get265:
     SHA256 fingerprint of the cert; in this case,
     specifically for use with ZNC 1.6"""
     def __init__(self, HostName, PortNum):
-        Host = HostName
-        Port = PortNum
-        ConnectTarget = (Host, Port)
+        #Give us some initial values
+        self.Host = HostName
+        self.Port = PortNum
+        #Connect to our target
+        ConnectTarget = (self.Host, self.Port)
+        #Get the certificate
         HostCert = ssl.get_server_certificate(ConnectTarget)
+        #Convert into DER from PEM format
         lines = HostCert.replace(" ",'').split()
-        der = a2b_base64(''.join(lines[1:-1]))        
+        der = a2b_base64(''.join(lines[1:-1]))
+        #Get the SHA256 sum of the DER cert
         sha256.update(der)
+        #Convert the sum into a printable format
         PrintableCert = sha256.hexdigest()
+        #Iterate over it to convert to a nice printable format
         splitter = iter(PrintableCert)
-        self.cert = FormattedCert = ':'.join(a+b for a,b in zip(splitter, splitter))        
-        
-#Define our host and port
-#TODO we'll be calling this via command line
-#Host = ('hosthosthosthosthost', 6697)
-#Grab the certificate and encode in utf-8
-#HostCert = ssl.get_server_certificate(Host)  #.encode('utf-8')
-#Convert from PEM to DER
-#lines = HostCert.replace(" ",'').split()
-#der = a2b_base64(''.join(lines[1:-1]))
-#Update checksum with the certificate
-#sha256.update(der)
-#Make it nice and printable, with semicolons between every second character
-#PrintableCert = sha256.hexdigest()
-#splitter = iter(PrintableCert)
-#FormattedCert = ':'.join(a+b for a,b in zip(splitter, splitter))
+        self.cert = FormattedCert = ':'.join(a+b for a,b in zip(splitter, splitter))
+
 google = Get265("google.com", 443)
+
 print(google.cert)
+print(google.Host)
+print(google.Port)
