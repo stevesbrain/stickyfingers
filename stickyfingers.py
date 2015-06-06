46e3e0ccad0476a4bdf5b6667920958f8df79640
StevesAMonkey <steve@tuxcon.com>
2015-06-06 23:20:44 +0930

    Refactored/restructured to make everything a small bit of the class. Don't know if this is best practice, but it seemed to be neater :)

--- stickyfingers.py
+++ stickyfingers.py
@@ -3,37 +3,44 @@ import ssl
 import hashlib
 from binascii import a2b_base64
 
-
-#Set the scene for our hashes
-sha256 = hashlib.sha256()
-
-
-class Get265:
+class CheckSum:
     """Takes the hostname/IP address
     and relevant port numbers, and returns a beautiful
     SHA256 fingerprint of the cert; in this case,
     specifically for use with ZNC 1.6"""
     def __init__(self, HostName, PortNum):
         #Give us some initial values
-        self.Host = HostName
-        self.Port = PortNum
-        #Connect to our target
-        ConnectTarget = (self.Host, self.Port)
+        self.HostName = HostName
+        self.PortNum = PortNum
+        ConnectTarget = (self.HostName, self.PortNum)
         #Get the certificate
         HostCert = ssl.get_server_certificate(ConnectTarget)
         #Convert into DER from PEM format
         lines = HostCert.replace(" ",'').split()
-        der = a2b_base64(''.join(lines[1:-1]))
+        self.der = der = a2b_base64(''.join(lines[1:-1]))
+    def formatString(self, cert):
+        """Formats whatever string you throw at it
+        in XX:XX:XX format. The Certificate is thrown
+        as the variable here."""
+        self.cert = cert
+        splitter = iter(cert)
+        certFormat = ':'.join(a+b for a,b in zip(splitter, splitter))
+        return(certFormat)
+    def Get256(self):
+        sha256 = hashlib.sha256()
         #Get the SHA256 sum of the DER cert
-        sha256.update(der)
+        sha256.update(self.der)
         #Convert the sum into a printable format
         PrintableCert = sha256.hexdigest()
-        #Iterate over it to convert to a nice printable format
-        splitter = iter(PrintableCert)
-        self.cert = FormattedCert = ':'.join(a+b for a,b in zip(splitter, splitter))
+        returnCert = self.formatString(PrintableCert)
+        print("SHA256 fingerprint:", returnCert)
+    def Get1(self):
+        sha1 = hashlib.sha1()
+        sha1.update(self.der)
+        PrintableCert = sha1.hexdigest()
+        returnCert = self.formatString(PrintableCert)
+        print("SHA1 fingerprint:", returnCert)
 
-google = Get265("google.com", 443)
-
-print(google.cert)
-print(google.Host)
-print(google.Port)
+google = CheckSum("google.com", 443)
+google.Get256()
+google.Get1()
