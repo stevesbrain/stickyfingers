1dcd6c5d0bbf872cc065eeed5f9da29573a2116d
StevesAMonkey <steve@tuxcon.com>
2015-06-06 21:38:57 +0930

    Checksum pre-DER certificate working. Need to convert to DER format.

--- stickyfingers.py
+++ stickyfingers.py
@@ -1,7 +1,15 @@
+#Import some libs and stuff
 import ssl
-from socket import socket
+import hashlib
+#from socket import socket
 
-s = socket()
-host = ('host', 6697)
-c = ssl.get_server_certificate(host)
-print(c)
+sha256 = hashlib.sha256()
+
+#s = socket()
+#Define our host and port
+#TODO we'll be calling this via command line
+Host = ('host', 6697)
+HostCert = ssl.get_server_certificate(Host).encode('utf-8')
+sha256.update(HostCert)
+PrintableCert = sha256.hexdigest()
+print(PrintableCert)
